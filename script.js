56e51dec637fe626ddcdc357d9d2887629eb1a52
iset20fires <cristinaveras0412@icloud.com>
2024-11-13 12:10:30 -0500

    complete

---
+++ script.js
@@ -0,0 +1,23 @@
+let house;
+
+let number = 100;
+
+if (number > 90){
+    house = "Grade A";
+    console.log (house);
+}
+
+else if (number > 80){
+    house = "Grade B"
+    console.log (house);
+}
+
+else if (number > 70){
+    house = "Grade C"
+    console.log (house);
+}
+
+else {
+    house ="Fail"
+    console.log (house);
+}
